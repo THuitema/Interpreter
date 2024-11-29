07c3878a7d682ba95d30b0ca36453fd3996ccd02
Thomas Huitema <thuitema35@gmail.com>
2024-11-28 22:44:35 -0500

    Basic shell to receive and print input from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to my interpreter!");
+
+    let stdin = io::stdin();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap(); // Required for Rust to print string without newline character
+
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        println!("{input}");
+
+        // Exit condition
+        if input == "q" || input == "quit" {
+            break;
+        }
+    }
+
+    println!("Goodbye...");
+
 }
