286a76ff5102f44b56b668d0dedbbc845d8628e5
Thomas Huitema <thuitema35@gmail.com>
2024-11-30 17:39:27 -0500

    Add Unary Minus token and implement Display trait

--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,32 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq, Clone)] // Derive PartialEq for comparison
 pub enum Token {
-  TokInt(i32),
+  TokInt(u32),
   TokFloat(f32),
+  TokUnaryMinus,
   TokPlus,
   TokMinus,
   TokMult,
   TokDiv,
   TokLParen,
   TokRParen
+}
+
+impl fmt::Display for Token {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match self {
+          Token::TokInt(n) => write!(f, "TokInt({})", n),
+          Token::TokFloat(d) => write!(f, "TokFloat({})", d),
+          Token::TokUnaryMinus => write!(f, "TokUnaryMinus"),
+          Token::TokPlus => write!(f, "TokPlus"),
+          Token::TokMinus => write!(f, "TokMinus"),
+          Token::TokMult => write!(f, "TokMult"),
+          Token::TokDiv => write!(f, "TokDiv"),
+          Token::TokLParen => write!(f, "TokLParen"),
+          Token::TokRParen => write!(f, "TokRParen"),
+      }
+  }
 }
 
 pub enum Expr {
